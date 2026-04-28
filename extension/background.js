4ae437f0f72b7cae3cb9131d5519da25b8c62188
Esteban <herranesteban9@gmail.com>
2026-04-28 17:58:58 -0500

    change url in background.js

--- extension/background.js
+++ extension/background.js
@@ -5,7 +5,7 @@ chrome.webNavigation.onBeforeNavigate.addListener(async (details) => {
         if (currentUrl.startsWith("chrome://") || currentUrl.startsWith("chrome-extension://") || currentUrl.startsWith("about:")) return;
 
         try {
-            let response = await fetch("http://127.0.0.1:8000/predict", {
+            let response = await fetch("https://ai-pishing.onrender.com/predict", {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify({ url: currentUrl })
